d24ca73991ba7c2058e04cf98a80eec5c9580be3
ismo1987 <122304355+ismo1987@users.noreply.github.com>
2023-01-11 19:49:58 +0100

    made changes to ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,8 @@
-resource "aws_instance" "jjtech" {
-  ami           = var.ami_id
+resource "aws_instance" "jjtech"
+  ami de Ismael = var.ami_id
   instance_type = var.instance_type
 
   tags = {
-    Name = "Mildred"
-  }
-}
+Name = "Ismael"
+  
+
